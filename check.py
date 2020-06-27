b54341b374cc7e840b607efa580cf1df5f848de8
Rob Stenson <rob.stenson@gmail.com>
2020-06-27 11:59:52 -0700

    update check for more generic behavior

--- check.py
+++ check.py
@@ -1,21 +1,33 @@
+import os
 from runpy import run_path
 from pathlib import Path
 import argparse, importlib, inspect, json, ast
 from pprint import pprint
 
 parser = argparse.ArgumentParser(prog="PBT", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("catalog", type=str)
+parser.add_argument("-f", "--file", type=str)
 args = parser.parse_args()
 
-catalog = "{:04d}".format(int(args.catalog))
-path = Path(f"plugins/{catalog}.py")
+#path = Path(f"plugins/{catalog}.py")
+#program = run_path(str(path))
 
-program = run_path(str(path))
+if args.file:
+    resolved = Path(args.file).resolve()
+    if resolved.is_dir():
+        files = resolved.glob("**/*.py")
+    else:
+        files = [resolved]
+else:
+    files = (Path(__file__).parent.resolve() / "translatables").glob("**/*.py")
 
-ts = program["ts"]
+for py in files:
+    print("\n\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n")
+    print(">>>", py)
+    program = run_path(str(py))
+    ts = program["ts"]
 
-for t in ts:
-    if t.warnings:
-        print("---")
-        pprint(t)
-        print(">>>>>>>>", t.warnings)
+    for t in ts:
+        if t.warnings:
+            print("---")
+            pprint(t)
+            print(">>>>>>>>", t.warnings)
