31a0df743ce3ef561bfbc997bbc1b63555f5cbd6
Rob Stenson <rob.stenson@gmail.com>
2020-06-27 16:08:55 -0700

    fix T

--- pbt/translations.py
+++ pbt/translations.py
@@ -23,62 +23,81 @@ class T():
         self.text = text
         self.context = context or None
         self.warnings = []
+        self.en(text)
     
     def __repr__(self):
         t = f"<T({self.tag})—{self.text}"
-        t += "".join(["("+str(getattr(self, lang))+")" for lang in LANGUAGE_SORT_ORDER[1:]])
+        #t += "".join(["("+str(getattr(self, lang))+")" for lang in LANGUAGE_SORT_ORDER[1:]])
         t += ">"
         return t
+    
+    def __getitem__(self, lang):
+        l = "_" + lang.replace("-", "")
+        if hasattr(self, l):
+            res = getattr(self, l)
+            if res == 0 or res == 1:
+                return self._en
+            else:
+                return res
+        else:
+            return self._en
 
     def en(self, translation):
-        self.en = translation
+        self._en = translation
         return self
     
     @translation
     def es(self, translation):
-        self.es = translation
+        self._es = translation
         return self
     
     @translation
     def fr(self, translation):
-        self.fr = translation
+        self._fr = translation
         return self
     
     @translation
     def pt(self, translation):
-        self.pt = translation
+        self._pt = translation
         return self
     
     @translation
     def ja(self, translation):
-        self.ja = translation
+        self._ja = translation
         return self
     
     @translation
     def ko(self, translation):
-        self.ko = translation
+        self._ko = translation
         return self
     
     @translation
     def zhHans(self, translation):
-        self.zhHans = translation
+        self._zhHans = translation
         return self
     
     @translation
     def zhHant(self, translation):
-        self.zhHant = translation
+        self._zhHant = translation
         return self
     
     @translation
     def ar(self, translation):
-        self.ar = translation
+        self._ar = translation
         return self
     
     @translation
     def he(self, translation):
-        self.he = translation
+        self._he = translation
         return self
     
 
-class TranslationSet(list):
-    pass
+class TranslationSet():
+    def __init__(self):
+        self.strings = {}
+
+    def append(self, item):
+        self.strings["_".join([item.tag, item.text, item.context or "None"])] = item
+
+    def lookup(self, tag, text, context):
+        return self.strings.get("_".join([tag, text, context or "None"]))
