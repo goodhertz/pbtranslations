be5fce15691c62a2b01eb58658dcb21c3e2ac7e2
Noah Dayan <noahdayan@gmail.com>
2020-12-14 15:54:44 +0000

    Add initial translations for 47

---
+++ translatables/plugins/GHZ0047/0047.py
@@ -0,0 +1,263 @@
+from pbt.translations import *
+
+
+ts = TranslationSet()
+
+ts.append(T(tag="ClumpLabel/text",
+    text='FILTER')
+    .es(0)
+    .pt(0)
+    .fr('FILTRE')
+    .ja('フィルター')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="ClumpLabel/text",
+    text='I')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('入')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="ClumpLabel/text",
+    text='O')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('出')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="ClumpLabel/text",
+    text='Tilt')
+    .es(0)
+    .pt(0)
+    .fr('Inclinaison')
+    .ja('チルト')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="ParamLabel/text",
+    text='Bias')
+    .es(0)
+    .pt(0)
+    .fr('Tension')
+    .ja('バイアス')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="ParamLabel/text",
+    text='Noise')
+    .es(0)
+    .pt(0)
+    .fr('Bruit')
+    .ja('ノイズ')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="ParamLabel/text",
+    text='dry')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('原音')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="FilterPosition",
+    text='Post')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('ポスト')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="FilterPosition",
+    text='Pre')
+    .es(0)
+    .pt(0)
+    .fr('Pré')
+    .ja('プリ')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TapeMode",
+    text='2 Track Hi')
+    .es(0)
+    .pt(0)
+    .fr('2 Pistes Hi')
+    .ja('2 Track ハイ')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TapeMode",
+    text='2 Track Lo')
+    .es(0)
+    .pt(0)
+    .fr('2 Pistes Lo')
+    .ja('2 Track ロー')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TapeMode",
+    text='C90 Hi')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('C90 ハイ')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TapeMode",
+    text='C90 Lo')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('C90 ロー')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TubeMode",
+    text='Tube X')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('チューブ X')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TubeMode",
+    text='Tube Y')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('チューブ Y')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TubeMode",
+    text='Tube Z')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('チューブ Z')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TubeTapeOrder",
+    text='Tube -> Tape')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('チューブ -> テープ')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Parameter/option",
+    context="TubeTapeOrder",
+    text='Tube <- Tape')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('チューブ <- テープ')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
+ts.append(T(tag="Tagline",
+    text='Tube + Tape = Tupe')
+    .es(0)
+    .pt(0)
+    .fr(1)
+    .ja('チューブ + テープ = チュープ')
+    .ko(0)
+    .zhHans(0)
+    .zhHant(0)
+    .ar(0)
+    .he(0)
+)
+
