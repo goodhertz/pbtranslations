f2123d7ab346ec77702d4adddee3b8e0e2119197
Noah Dayan <noahdayan@gmail.com>
2023-08-23 12:52:48 +0100

    Add new line

--- translatables/plugins/releases/GHZ0029/0029.py
+++ translatables/plugins/releases/GHZ0029/0029.py
@@ -669,8 +669,8 @@ ts.append(T(tag="ParamLabel/text",
 ts.append(T(tag="ClumpLabel/text",
     text='Trim Glide\nTimes')
     .es(0)
-    .pt('Tempo de Deslizamento')
-    .fr('Temps de Balayage')
+    .pt('Tempo de\nDeslizamento')
+    .fr('Temps de\nBalayage')
     .it(0)
     .ja('トリム\nグライド')
     .ko('트림\n글라이드')
